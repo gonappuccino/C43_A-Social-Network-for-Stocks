799b11da4e23e498256345631438247beae95b40
JS Dandurand <jsdandurand@hotmail.com>
2025-04-11 21:53:37 -0400

    deleted unused files

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .prediction_model import StockPredictionModel
-from .portfolio_analysis import PortfolioAnalyzer
-from .review_model import ReviewManager
-
-# Create instances of prediction model and portfolio analyzer
-prediction_model = StockPredictionModel()
-portfolio_analyzer = PortfolioAnalyzer()
-review_manager = ReviewManager() 
